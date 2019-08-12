71020b05ac1b3017f6958bbaa163d51226c1c9bd
handikahevn <handikahevn24@gmail.com>
2019-08-12 07:58:38 +0700

    Penambaha Pada Dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,21 @@
 <?php
 include '../config/db.php';
+$sqlfaktur = "SELECT * FROM pembelian group by no_faktur";
+$rfaktur = $con->query($sqlfaktur);
+$rowfaktur = $rfaktur->num_rows;
+$sqlsupplier = "SELECT * FROM supplier group by kode_supplier";
+$rsupplier = $con->query($sqlsupplier);
+$rowsupplier = $rsupplier->num_rows;
+$sqlbarang = "SELECT * FROM barang group by kode_barang";
+$rbarang = $con->query($sqlbarang);
+$rowbarang = $rbarang->num_rows;
+$sqlpenanggungjawab = "SELECT * FROM penanggung group by nama";
+$rpenanggung = $con->query($sqlpenanggungjawab);
+$rowpenanggungjawab = $rpenanggung->num_rows;
+$sqlrusakringan = "SELECT sum(rusak_ringan) as rusak_ringan, sum(hilang) as hilang, sum(rusak_berat) as rusak_berat FROM keadaan_barang";
+$rrusakringan = $con->query($sqlrusakringan);
+$datakeadaan_barang = $rrusakringan->fetch_assoc();
+
 ?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
@@ -71,12 +87,6 @@ include '../config/db.php';
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="laporan.php">
-                  <span data-feather="bar-chart-2"></span>
-                  Laporan
-                </a>
-              </li>
-              <li class="nav-item">
                 <a class="nav-link" href="pengaturanuser.php">
                   <span data-feather="settings"></span>
                   Pengaturan User
@@ -89,6 +99,52 @@ include '../config/db.php';
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
           </div>
+          <div class="row border border-dark text-center">
+              <div class="col-md-4 border border-dark" style="background-color:#11aab3;">
+                <h3>Jumlah Faktur</h3>
+                <br />
+                <p style="font-size:70px;"><?=$rowfaktur;?></p>
+              </div>
+              <div class="col-md-4 border border-dark" style="background-color:#80ffff;">
+              <h3>
+                Jumlah Barang
+              </h3>
+              <br />
+                <p style="font-size:70px;"><?=$rowbarang;?></p>
+              </div>
+              <div class="col-md-4 border border-dark" style="background-color:#c4ffff;">
+                <h3>Jumlah Supplier</h3>
+                <br />
+                <p style="font-size:70px;"><?=$rowsupplier;?></p>
+              </div>
+          </div>
+          <div class="row border border-dark">
+              <div class="col-md-4 border border-dark text-center"style="background-color:#c4ffff;">
+              <h3>Jumlah Penanggung Jawab</h3>
+              <br />
+                <p style="font-size:70px;"><?=$rowpenanggungjawab;?></p>
+              </div>
+              <div class="col-md-8 border border-dark text-center" style="background-color:#80ffff;">
+              <h3>Keadaan Barang</h3>
+                <div class="row text-center">
+                  <div class="col-md-4">
+                    <h5>Hilang</h5>
+                    <br />
+                    <p style="font-size:70px;"><?=$datakeadaan_barang['hilang'];?></p>
+                  </div>
+                  <div class="col-md-4">
+                    <h5>Rusak Ringan</h5>
+                    <br />
+                    <p style="font-size:70px;"><?=$datakeadaan_barang['rusak_ringan'];?></p>
+                  </div>
+                  <div class="col-md-4">
+                  <h5>Rusak Berat</h5>
+                  <br />
+                    <p style="font-size:70px;"><?=$datakeadaan_barang['rusak_berat'];?></p>
+                  </div>
+                </div>
+              </div>
+            </div>
 
           <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
 

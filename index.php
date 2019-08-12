23565264a9ab3d1472676316bcdf8f768928411c
handikahevn <handikahevn24@gmail.com>
2019-08-12 07:51:15 +0700

    Add Offline Dist

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
         <link rel="stylesheet" href="">
         <style>
 
@@ -188,6 +185,10 @@ body, html {
         </div><!-- /card-container -->
     </div><!-- /container -->
 
+
+<?php
+include"admin/js.php";
+?>
 <script>
 $( document ).ready(function() {
     // DOM ready

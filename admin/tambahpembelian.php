b2145d41a635358400be793988d60a535f069298
handikahevn <handikahevn24@gmail.com>
2019-08-12 07:59:13 +0700

    Perubahan tambah pembelian tanpa detail

--- admin/tambahpembelian.php
+++ admin/tambahpembelian.php
@@ -2,15 +2,15 @@
 include '../config/db.php';
 
 if (isset($_POST['simpan'])) {
-    $no_faktur = $_POST['no_faktur'];
-    $supplier = $_POST['supplier'];
+    $no_faktur = $_GET['no_faktur'];
+    $supplier = $_GET['supplier'];
     $barang = $_POST['barang'];
-    $tanggal_pembelian = $_POST['tanggal_pembelian'];
+    $tanggal_pembelian = $_GET['tanggal_pembelian'];
     $jumlah_barang = $_POST['jumlah_barang'];
     $harga_satuan = $_POST['harga_satuan'];
     $harga_total = $_POST['harga_total'];
     
-    $sql = "INSERT INTO detailpembelian (no_faktur, kode_supplier, kode_barang, tanggal_pembelian, jumlah_barang, harga_satuan, harga_total)
+    $sql = "INSERT INTO pembelian (no_faktur, kode_supplier, kode_barang, tanggal_pembelian, jumlah_barang, harga_satuan, harga_total)
     VALUES ('$no_faktur', '$supplier', '$barang','$tanggal_pembelian', '$jumlah_barang', '$harga_satuan','$harga_total')";
 
     if ($con->query($sql) === TRUE) {
@@ -20,11 +20,13 @@ if (isset($_POST['simpan'])) {
             // Jika barang belum ada
             $sql2 = "INSERT INTO keadaan_barang (kode_barang,jumlah) VALUES ('$barang','$jumlah_barang')";
             $con->query($sql2);
+        }else{
+
+            $sql3 = "UPDATE keadaan_barang set jumlah = jumlah + '$jumlah_barang' where kode_barang = '$barang'";
+            $con->query($sql3);
         }
         // Jika Barang sudah ada
-        $sql3 = "UPDATE keadaan_barang set jumlah = jumlah + '$jumlah_barang' where kode_barang = '$barang'";
-        $con->query($sql3);
-        //header('location: pembelian.php');
+        header('location: detailpembelian.php?no_faktur='.$no_faktur.'&supplier='.$supplier.'&tanggal_pembelian='.$tanggal_pembelian);
     } else {
         echo "Error: " . $sql . "<br>" . $con->error;
     }
